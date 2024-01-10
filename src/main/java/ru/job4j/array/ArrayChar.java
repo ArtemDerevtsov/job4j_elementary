91ec630db128aea2a2b97b8d130de6e78a8df0d6
ArtemDerevtsov <a.derevcov@mail.ru>
2024-01-11 00:35:05 +0700

    6.4.0. Слово начинается с ... [#41585 #471980]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,7 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
         for (int index = 0; index < prefix.length; index++) {
-            if ((prefix.length <= word.length) && (prefix[index] != word[index])) {
+            if (prefix[index] != word[index]) {
                 result = false;
                 break;
             }
