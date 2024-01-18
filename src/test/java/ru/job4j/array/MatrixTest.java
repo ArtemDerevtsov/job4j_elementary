ec4ba11aaee80ddc7bbbd70ae788ec2ef70b245e
ArtemDerevtsov <a.derevcov@mail.ru>
2024-01-19 02:40:43 +0700

    6.6.4. Двухмерный массив. Таблица умножения. [#33491]

---
+++ src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,122 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MatrixTest {
+    @Test
+    public void when2on2() {
+        int size = 2;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
+                {1, 2},
+                {2, 4}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+
+    @Test
+    public void when3on3() {
+        int size = 3;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
+                {1, 2, 3},
+                {2, 4, 6},
+                {3, 6, 9}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+
+    @Test
+    public void when4on4() {
+        int size = 4;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
+                {1, 2, 3, 4},
+                {2, 4, 6, 8},
+                {3, 6, 9, 12},
+                {4, 8, 12, 16}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+
+    @Test
+    public void when5on5() {
+        int size = 5;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
+                {1, 2, 3, 4, 5},
+                {2, 4, 6, 8, 10},
+                {3, 6, 9, 12, 15},
+                {4, 8, 12, 16, 20},
+                {5, 10, 15, 20, 25}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+
+    @Test
+    public void when6on6() {
+        int size = 6;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
+                {1, 2, 3, 4, 5, 6},
+                {2, 4, 6, 8, 10, 12},
+                {3, 6, 9, 12, 15, 18},
+                {4, 8, 12, 16, 20, 24},
+                {5, 10, 15, 20, 25, 30},
+                {6, 12, 18, 24, 30, 36}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+
+    @Test
+    public void when7on7() {
+        int size = 7;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
+                {1, 2, 3, 4, 5, 6, 7},
+                {2, 4, 6, 8, 10, 12, 14},
+                {3, 6, 9, 12, 15, 18, 21},
+                {4, 8, 12, 16, 20, 24, 28},
+                {5, 10, 15, 20, 25, 30, 35},
+                {6, 12, 18, 24, 30, 36, 42},
+                {7, 14, 21, 28, 35, 42, 49}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+
+    @Test
+    public void when8on8() {
+        int size = 8;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {2, 4, 6, 8, 10, 12, 14, 16},
+                {3, 6, 9, 12, 15, 18, 21, 24},
+                {4, 8, 12, 16, 20, 24, 28, 32},
+                {5, 10, 15, 20, 25, 30, 35, 40},
+                {6, 12, 18, 24, 30, 36, 42, 48},
+                {7, 14, 21, 28, 35, 42, 49, 56},
+                {8, 16, 24, 32, 40, 48, 56, 64}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+
+    @Test
+    public void when9on9() {
+        int size = 9;
+        int[][] result = Matrix.multiple(size);
+        int[][] expected = {
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {2, 4, 6, 8, 10, 12, 14, 16, 18},
+                {3, 6, 9, 12, 15, 18, 21, 24, 27},
+                {4, 8, 12, 16, 20, 24, 28, 32, 36},
+                {5, 10, 15, 20, 25, 30, 35, 40, 45},
+                {6, 12, 18, 24, 30, 36, 42, 48, 54},
+                {7, 14, 21, 28, 35, 42, 49, 56, 63},
+                {8, 16, 24, 32, 40, 48, 56, 64, 72},
+                {9, 18, 27, 36, 45, 54, 63, 72, 81}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+}
